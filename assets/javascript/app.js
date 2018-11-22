cf339afa51772d83f34b556d5a37847f28ab5294
Dylan Thomson <djthomson88@gmail.com>
2018-11-22 15:42:08 -0500

    Move timer logic inside TriviaGame, handle missed answers, handle gameOver

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,32 +25,32 @@ Timer
 -Time, time remaining, interval ID
 -Start, stop, reset
 *****************************************/
-class GameTimer {
-    constructor(time) {
-        this.time = time;
-        this.timeRemaining = time;
-        this.intervalId;
-    }
+// class GameTimer {
+//     constructor(time) {
+//         this.time = time;
+//         this.timeRemaining = time;
+//         this.intervalId;
+//     }
 
-    startTimer() {
-        var self = this;
-        self.intervalId = setInterval(function() {
-            console.log(self.timeRemaining);
-            self.timeRemaining--;
-            if(self.timeRemaining < 0) {
-                self.stopTimer();
-            }
-        }, 1000);
-    }
+//     startTimer() {
+//         var self = this;
+//         self.intervalId = setInterval(function() {
+//             console.log(self.timeRemaining);
+//             self.timeRemaining--;
+//             if(self.timeRemaining < 0) {
+//                 self.stopTimer();
+//             }
+//         }, 1000);
+//     }
     
-    stopTimer() {
-        clearInterval(this.intervalId);
-    }
+//     stopTimer() {
+//         clearInterval(this.intervalId);
+//     }
 
-    resetTimer() {
-        this.timeRemaining = this.time;
-    }
-}
+//     resetTimer() {
+//         this.timeRemaining = this.time;
+//     }
+// }
 
 /****************************************
 TriviaGame
@@ -60,32 +60,64 @@ TriviaGame
 -Correct and wrong answer count
 *****************************************/
 class TriviaGame {
-    constructor(questions, timer) {
+    constructor(questions, time) {
         this.questions = questions;
-        this.timer = timer;
+        this.time = time;
+        this.timeRemaining = time;
         this.currentQuestion = this.questions.shift();
         console.log(this.currentQuestion.question);
         this.correctAnswers = 0; 
-        this.incorrectAnswers = 0;
+        this.missedAnswers = 0;
+        this.intervalId;
     }
 
     nextQuestion() {
         if(this.questions.length > 0) {
             this.currentQuestion = this.questions.shift();
             console.log(this.currentQuestion.question);
+            return this.currentQuestion;
         }
         else {
-            this.win();
+            this.gameOver();
+            return null;
         }
     }
 
-    win() {
-        console.log("You won!");
+    gameOver() {
+        console.log("Game Over!");
         console.log("Correct answers", this.correctAnswers);
-        console.log("Incorrect answers", this.incorrectAnswers);
-
+        console.log("Missed answers", this.missedAnswers);
     }
 
+    startTimer() {
+        this.intervalId = setInterval(() => {
+            console.log(this.timeRemaining);
+            this.timeRemaining--;
+            if(this.timeRemaining < 0) {
+                this.missedAnswer();
+            }
+        }, 1000);
+    }
+
+    stopTimer() {
+        clearInterval(this.intervalId);
+    }
+
+    resetTimer() {
+        this.timeRemaining = this.time;
+    }
+
+    missedAnswer() {
+        this.stopTimer();
+        this.missedAnswers++;
+        console.log("Nope. The correct answer is: " + this.currentQuestion.answers[this.currentQuestion.indexOfAnswer]);
+        setTimeout(() => {
+            if(this.nextQuestion()) {
+                this.resetTimer();
+                this.startTimer();
+            }
+        }, 5 * 1000);
+    }
 
 }
 
@@ -97,8 +129,8 @@ function init() {
         new TriviaQuestion("Do you like bread?", ["Yes", "Maybe", "No"], 1, "image"),
         new TriviaQuestion("Do you like kale?", ["Yes", "Maybe", "No"], 2, "image"),
     ];
-    var timer = new GameTimer(60);
-    game = new TriviaGame(questions, timer);
+    // var timer = new GameTimer(60);
+    game = new TriviaGame(questions, 3);
 }
 
 $(document).ready(function() {
