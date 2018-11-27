cc12dcca8eaf89713dc0d8c7c9873d504606e396
Dylan Thomson <djthomson88@gmail.com>
2018-11-26 19:46:32 -0500

    Add Office questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,7 +65,7 @@ class TriviaGame {
                 this.resetTimer();
                 this.startTimer();
             }
-        }, 3 * 1000);
+        }, 5 * 1000);
     }
     
     nextQuestion() {
@@ -147,17 +147,49 @@ var game;
 
 function init() {
     var questions = [
-        new TriviaQuestion("Do you like cheese?", ["Yes", "Maybe", "No"], 0, "https://amp.businessinsider.com/images/5b8592ba89c8a1d6218b4a36-750-563.jpg"),
-        new TriviaQuestion("Do you like bread?", ["Yes", "Maybe", "No"], 1, "https://www.tasteofhome.com/wp-content/uploads/2018/01/exps32480_MRR153791D09_18_6b-2-696x696.jpg"),
-        new TriviaQuestion("Do you like kale?", ["Yes", "Maybe", "No"], 2, "https://i0.wp.com/www.healthline.com/hlcmsresource/images/AN_images/benefits-of-kale-1296x728-feature.jpg?w=1155&h=1528"),
+        new TriviaQuestion("Pam Beesly received a lifetime ban from which of the following restaurants?", 
+                          ["Applebee's", "Chili's", "Chipotle", "McDonald's"], 
+                          1, 
+                          "https://media.giphy.com/media/yoJC2oCKxTLNrr30Jy/giphy.gif"),
+        new TriviaQuestion("Michael Scott organized a charity race to raise funds in order to cure which of the following diseases?", 
+                          ["Polio", "Affluenza", "Tourette's Syndrome", "Rabies"], 
+                          3, 
+                          "https://media.giphy.com/media/p5tWDtmnHZyY8/giphy.gif"),
+        new TriviaQuestion("Kevin Malone is the lead singer and drummer of which local band?", 
+                          ["Scrantonicity", "Kevin and the Heartbreakers", "The Bacon Brothers", "Maloneum"], 
+                          0, 
+                          "https://media.giphy.com/media/yaQ8O1vftVWb6/giphy.gif"),
+        new TriviaQuestion("What is Creed Bratton's position at Dunder Mifflin?", 
+                          ["Accountant", "He doesn't actually work there", "Quality Assurance", "Salesman"], 
+                          2, 
+                          "https://media.giphy.com/media/JLnJ99KsV4Nq0/giphy.gif"),
+        new TriviaQuestion("What substance does Jim Halpert put office supplies into as a prank?", 
+                          ["Pudding", "Jello", "Super Glue", "Butter"], 
+                          1, 
+                          "https://media.giphy.com/media/I2mwW3cPptosg/giphy.gif"),
+        new TriviaQuestion("What is Ryan Howard's nickname?", 
+                          ["Big Tuna", "Ry Ry", "Fire Guy", "Bat Boy"], 
+                          2, 
+                          "https://media.giphy.com/media/jA4T01RxBv77W/giphy.gif"),
+        new TriviaQuestion("What does Stanley Hudson love more than anything else in this world?", 
+                          ["His job", "His dog", "Red wine", "Pretzel Day"], 
+                          3, 
+                          "https://media.giphy.com/media/vgUFOWBwBkziE/giphy.gif"),
+        new TriviaQuestion("Dwight Schrute is the owner of Schrute Farms, where he grows which crop?", 
+                          ["Carrots", "Potatoes", "Beans", "Beets"], 
+                          3, 
+                          "https://media.giphy.com/media/134DVXcD94sOWI/giphy.gif"),
+        new TriviaQuestion("Which of the following is NOT the name of one of Angela Martin's cats?", 
+                          ["Lady Aragorn", "Veronica", "Mr. Ash", "Pawlick Baggins"], 
+                          1, 
+                          "https://media.giphy.com/media/ibULBaRu6iq1a/giphy.gif")
     ];
-    // var timer = new GameTimer(60);
-    game = new TriviaGame(questions, 3);
+
+    game = new TriviaGame(questions, 10);
 }
 
 $(document).ready(function() {
     init();
-    // game.start();
 
     $("#start-btn").on("click", () => {
         game.start();
